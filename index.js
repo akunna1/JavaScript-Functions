6404fbaaebb629c46462016bf4a3f0899a5e96cd
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2024-11-13 11:24:26 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// i=1 is the starting point, i<=100 is the limit, i++ is to add one continuously, the last part is in {}
+for (let i=1; i<=100; i++) {
+  //console.log(i);
+}
+
+// k=2 is the starting point, k<=20 is the limit, i+=2 is to add two continuously
+for (let k=2; k<=20; k+=2) {
+  //console.log(k);
+}
+
+// x=1 is the starting point, x<=20 is the limit, x++ is to add one continuously
+for (let x = 1; x <= 20; x++) {
+    let y = 2 * x ** 2 + 4 * x + 6; // Calculate y using the given formula, y=2x^2 + 4x + 6
+    console.log(`For x = ${x}, y = ${y}`);
+}
